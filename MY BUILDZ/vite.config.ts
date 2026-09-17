cca0daf79969810ceb9a615f7947a7acc7b1ecb2
theprogrammingmind <heritageayowumi111@gmail.com>
2026-09-17 09:15:03 +0100

    Blah Blah Blah

--- MY BUILDZ/vite.config.ts
+++ MY BUILDZ/vite.config.ts
@@ -1,6 +1,16 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
-    plugins: [tailwindcss()],
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(import.meta.dirname, "index.html"),       
+        project2: resolve(import.meta.dirname, "Main_Project_2.html"), 
+        project3: resolve(import.meta.dirname, "Resturant.html"), 
+      },
+    },
+  },
 });
